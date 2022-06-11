1971ded8760a9ee70832f113511db5016d372dc7
brenomorais-ti <brenowebdesigner@gmail.com>
2022-06-11 13:26:40 -0300

    create Data item and Type Item, create Data category and Type Category

--- financial-control/src/data/items.ts
+++ financial-control/src/data/items.ts
@@ -1,5 +1,7 @@
-export const items = [
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
     { date: new Date(2021, 9, 15),  category: 'food', title: 'McDonalds', value: 32.12},
     { date: new Date(2021, 6, 12),  category: 'rent', title: 'Aluguel Apt', value: 1000.00},
-    { date: new Date(2021, 9, 20),  category: 'salary', title: 'Salário', value: 5000.00},
+    { date: new Date(2021, 9, 20 ),  category: 'salary', title: 'Salário', value: 5000.00},
 ];
---
+++ financial-control/src/types/Categoty.ts
@@ -0,0 +1,7 @@
+export type Category = {
+   [tag: string ]: { /*Para que possa ser criado com qual quer nome*/
+        title: string;
+        color: string;
+        expense: boolean;
+   } 
+}
