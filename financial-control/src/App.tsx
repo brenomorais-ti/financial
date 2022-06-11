be39f683a0affef85316c1e27d794a41cb737919
brenomorais-ti <brenowebdesigner@gmail.com>
2022-06-11 13:49:22 -0300

    create folder helpers and file dateFilter

--- financial-control/src/App.tsx
+++ financial-control/src/App.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Categoty';
+import { categories } from './data/categories';
+import { items } from './data/items';
+import { getCurrentMonth } from './helpers/dateFilter';
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth);
+
   return (
     <C.Container>
       <C.Header>
@@ -14,7 +23,7 @@ const App = () => {
         {/*Área de inserção*/}
         
         {/*Tabela de itens*/}
-        
+
       </C.Body>
     </C.Container>
   );
